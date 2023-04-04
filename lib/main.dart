b7d92affd6d5d4bab124b24e14f441a88b8320bb
Ata Meydani <miratameydani@gmail.com>
2023-04-04 18:01:07 +0330

    Add the ability to search tasks

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,7 @@ class MyApp extends StatelessWidget {
           labelStyle: TextStyle(color: secondaryTextColor),
           border: InputBorder.none,
           iconColor: secondaryTextColor,
+          floatingLabelBehavior: FloatingLabelBehavior.never,
         ),
         colorScheme: const ColorScheme.light(
           primary: primaryColor,
@@ -58,13 +59,16 @@ class MyApp extends StatelessWidget {
           onSecondary: Colors.white,
         ),
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  HomeScreen({Key? key}) : super(key: key);
+
+  final TextEditingController _controller = TextEditingController();
+  final ValueNotifier<String> _searchKeywordNotifier = ValueNotifier('');
 
   @override
   Widget build(BuildContext context) {
@@ -135,8 +139,12 @@ class HomeScreen extends StatelessWidget {
                           )
                         ],
                       ),
-                      child: const TextField(
-                        decoration: InputDecoration(
+                      child: TextField(
+                        onChanged: (value) {
+                          _searchKeywordNotifier.value = _controller.text;
+                        },
+                        controller: _controller,
+                        decoration: const InputDecoration(
                           prefixIcon: Icon(CupertinoIcons.search),
                           label: Text('Search tasks...'),
                         ),
@@ -147,65 +155,76 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             Expanded(
-              child: ValueListenableBuilder<Box<TaskEntity>>(
-                valueListenable: box.listenable(),
-                builder: (context, box, child) {
-                  return box.isNotEmpty
-                      ? ListView.builder(
-                          padding: const EdgeInsets.fromLTRB(16, 16, 16, 100),
-                          itemCount: box.values.length + 1,
-                          itemBuilder: (context, index) {
-                            if (index == 0) {
-                              return Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+              child: ValueListenableBuilder(
+                valueListenable: _searchKeywordNotifier,
+                builder: (context, value, child) {
+                  return ValueListenableBuilder<Box<TaskEntity>>(
+                    valueListenable: box.listenable(),
+                    builder: (context, box, child) {
+                      final List items;
+                      if (_controller.text.isEmpty) {
+                        items = box.values.toList();
+                      } else {
+                        items = box.values.where((task) => task.name.contains(_controller.text)).toList();
+                      }
+                      return items.isNotEmpty
+                          ? ListView.builder(
+                              padding: const EdgeInsets.fromLTRB(16, 16, 16, 100),
+                              itemCount: items.length + 1,
+                              itemBuilder: (context, index) {
+                                if (index == 0) {
+                                  return Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text(
-                                        'Today',
-                                        style: themeData.textTheme.titleLarge!.apply(fontSizeFactor: 0.9),
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Today',
+                                            style: themeData.textTheme.titleLarge!.apply(fontSizeFactor: 0.9),
+                                          ),
+                                          Container(
+                                            width: 70,
+                                            height: 3,
+                                            margin: const EdgeInsets.only(top: 4),
+                                            decoration: BoxDecoration(
+                                              color: primaryColor,
+                                              borderRadius: BorderRadius.circular(1.5),
+                                            ),
+                                          )
+                                        ],
                                       ),
-                                      Container(
-                                        width: 70,
-                                        height: 3,
-                                        margin: const EdgeInsets.only(top: 4),
-                                        decoration: BoxDecoration(
-                                          color: primaryColor,
-                                          borderRadius: BorderRadius.circular(1.5),
+                                      MaterialButton(
+                                        color: const Color(0xffEAEFF5),
+                                        textColor: secondaryTextColor,
+                                        elevation: 0,
+                                        onPressed: () {
+                                          box.clear();
+                                        },
+                                        child: Row(
+                                          children: const [
+                                            Text('Delete All'),
+                                            SizedBox(
+                                              width: 4,
+                                            ),
+                                            Icon(
+                                              CupertinoIcons.delete_solid,
+                                              size: 18,
+                                            ),
+                                          ],
                                         ),
-                                      )
+                                      ),
                                     ],
-                                  ),
-                                  MaterialButton(
-                                    color: const Color(0xffEAEFF5),
-                                    textColor: secondaryTextColor,
-                                    elevation: 0,
-                                    onPressed: () {
-                                      box.clear();
-                                    },
-                                    child: Row(
-                                      children: const [
-                                        Text('Delete All'),
-                                        SizedBox(
-                                          width: 4,
-                                        ),
-                                        Icon(
-                                          CupertinoIcons.delete_solid,
-                                          size: 18,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              );
-                            } else {
-                              final TaskEntity task = box.values.toList()[index - 1];
-                              return TaskItem(task: task);
-                            }
-                          },
-                        )
-                      : const EmptyState();
+                                  );
+                                } else {
+                                  final TaskEntity task = items[index - 1];
+                                  return TaskItem(task: task);
+                                }
+                              },
+                            )
+                          : const EmptyState();
+                    },
+                  );
                 },
               ),
             ),
